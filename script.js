b8e519a67588c2b9496fb37d42783204112fdad4
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-03-17 16:02:17 +0700

    before using bootstrap scrollspy

---
+++ script.js
@@ -0,0 +1,12 @@
+$('a[href^="#"]').click(function(e){
+
+var target = $(this).attr("href");
+var strip = target.slice(1);
+var anchor = $("a[name=' "+ strip + "']");
+e.preventDefault();
+
+$('html, body').animate({
+  scrollTop: anchor.offset().top
+}, 2000 );
+
+})
